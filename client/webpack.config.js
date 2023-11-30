caad0d81496483a358f7f0e631fc19e2cc9cf51c
Howard Pifer <paradx34@outlook.com>
2023-11-29 20:04:01 -0500

    js update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
+// Imports
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add CSS loaders and babel to webpack.
 
