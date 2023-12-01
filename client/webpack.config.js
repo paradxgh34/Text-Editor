169bfcfc027879f6090bbf784c6a82a50a282a8e
Howard Pifer <paradx34@outlook.com>
2023-11-30 21:09:55 -0500

    update to js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add CSS loaders and babel to webpack.
 // Workbox plugins for service worker and manifest file
 module.exports = () => {
   return {
@@ -17,9 +16,6 @@ module.exports = () => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
-//////////
-    // Webpack plugin 
-    // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
       // Webpack plugin that generates our html file and injects our bundles
       new HtmlWebpackPlugin({
@@ -33,32 +29,26 @@ module.exports = () => {
         swDest: "src-sw.js",
       }),
 
-      ////////////////////
-
-
-
-
-           // Creates a manifest.json file
-           new WebpackPwaManifest({
-            fingerprints: false,
-            inject: true,
-            name: "Text Editor",
-            short_name: "Text",
-            description: "Keep you notes here!",
-            background_color: "#225ca3",
-            theme_color: "#225ca3",
-            start_url: "/",
-            publicPath: "/",
-            icons: [
-              {
-                src: path.resolve("src/images/logo.png"),
-                sizes: [96, 128, 192, 256, 384, 512],
-                destination: path.join("assets", "icons"),
-              },
-            ],
-          }),
+      // Creates a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Text Editor",
+        short_name: "Text",
+        description: "Keep you notes here!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
         ],
-        ////////////////////////////////////
+      }),
+    ],
 
     // CSS loaders and babel to webpack
     module: {
@@ -83,9 +73,3 @@ module.exports = () => {
     },
   };
 };
-
-////////////////////////
-
-
-
-// TODO: Add CSS loaders and babel to webpack.
